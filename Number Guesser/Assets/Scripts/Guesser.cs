2f357ad8521bcc07b04fa6e98f6de5d794fc8f9a
Unknown <kylehinckley@hotmail.com>
2018-01-19 09:55:29 -0700

    Number Guesser v0.4
    Added new games, player wins, fixed max cap.

--- Number Guesser/Assets/Scripts/Guesser.cs
+++ Number Guesser/Assets/Scripts/Guesser.cs
@@ -3,37 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Guesser : MonoBehaviour {
-    public int max = 1000;
-    public int min = 1;
-    public int half = 2;
-    public int guess;
+    public int max;
+    public int min;
+    public int count;
+    private int newMax;
+    private int newMin;
+    private int newCount;
+    public const int HALF = 2;
+    private int guess;
     // Use this for initialization
     private void Start () {
-        
+        //Intro Dialogue
+        newMax = max;
+        newMin = min;
+        newCount = count;
         print("Welcome to Number Guesser!");
-        print ("Pick a number between " + min + " and " + max + " inclusive.");
+        print ("Pick a number between " + (min) + " and " + (max) + " inclusive.");
         
-        // Is the value var guess?
-        print("Is the number " + guess + "?");
         // Instructions
         print("Press the up arrow for higher, press the down arrow for lower, and hit enter if I got it!");
-	}
+        NextGuess();
+        max++;
+    }
+
+    private void NextGuess() {
+        count--;
+        guess = (min + max) / HALF;
+        print("Is the number " + guess + "?");
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        // Press Up Key
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             min = guess;
-            guess = (min + max) / half;
-            print("Is the number " + guess + "?");
+            NextGuess();
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
+        // Press Down Key
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             max = guess;
-            guess = (min + max) / half;
-            print("Is the number " + guess + "?");
+            NextGuess();
         }
-        if (Input.GetKeyDown(KeyCode.Return)) {
-            print("Nice.");
+        // Press Return
+        else if (Input.GetKeyDown(KeyCode.Return)) {
+            print("I have achieved victory. In your face.\n\n\n");
+            max = newMax;
+            min = newMin;
+            count = newCount;
+            Start();
+        }
+        else if (count == 0)
+        {
+            print("I have brought shame to my circuits. In my face. T.T\n\n\n");
+            max = newMax;
+            min = newMin;
+            count = newCount;
+            Start();
         }
     }
 }
