ef6927ab4c49d59fd13131dae328c0138ae23ba4
Unknown <kylehinckley@hotmail.com>
2018-02-05 09:51:04 -0700

    BrickBreaker v0.2

---
+++ BrickBreaker/Assets/DontDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    public class Music : MonoBehaviour
+    {
+        void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+}
