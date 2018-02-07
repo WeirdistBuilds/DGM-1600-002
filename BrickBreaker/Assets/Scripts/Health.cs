1d8cf1b4dfb1652657cf04f4a80ba0d20edcacbb
Unknown <kylehinckley@hotmail.com>
2018-02-07 09:51:25 -0700

    BrickBreaker v0.3
    Added collision for ball and brick, health for brick.

---
+++ BrickBreaker/Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+
+        // if brick health is zero, brick is destroyed
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
