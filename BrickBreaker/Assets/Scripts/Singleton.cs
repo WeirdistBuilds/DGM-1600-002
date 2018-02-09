6847d75f9a553738ea5560d7bc35bd4eb2eed5c9
Unknown <kylehinckley@hotmail.com>
2018-02-09 09:53:25 -0700

    BrickBreaker v0.4
    Added brick prefab, boundaries, collision layers

---
+++ BrickBreaker/Assets/Scripts/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+
+    public static Singleton instance = null;
+
+
+	void Awake () {
+		if (instance == null)                           // if instance is not assigned, assign to this object
+        {
+            instance = this;
+        }
+        else if (instance != this)                       // if instance doesn't equal this, destroy this
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);             // don't destroy on load
+
+	}
+}
