79b006487b323dd26ce3fdcd1b59676e783b413a
Unknown <kylehinckley@hotmail.com>
2018-02-16 09:52:19 -0700

    BrickBreaker v0.6
    Diddled with GameManager, added Game Over screen.

---
+++ BrickBreaker/Assets/Scripts/BoundaryBottom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryBottom : MonoBehaviour {
+
+    public GameManager theGameManager;
+
+    
+
+    void Start () {
+        theGameManager = FindObjectOfType<GameManager>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("gameover");
+        theGameManager.LoadLevel("GameOver");
+    }
+}
---
+++ BrickBreaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+
+    void Awake()
+    {
+        if (instance == null)                           // if instance is not assigned, assign to this object
+        {
+            instance = this;
+        }
+        else if (instance != this)                       // if instance doesn't equal this, destroy this
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);             // don't destroy on load
+
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
