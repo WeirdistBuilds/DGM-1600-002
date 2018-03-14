39691622457b2af93a2cf224efaff6048656d04b
Unknown <kylehinckley@hotmail.com>
2018-03-14 02:00:47 -0600

    2D Platformer v0.1
    Rudiments of platformer created, using placeholder sprites.

---
+++ 2D Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject focus;
+    public float leadSpace;
+    public float transitionTime;
+
+    private Vector3 focusPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        focusPosition = new Vector3 (focus.transform.position.x, transform.position.y, transform.position.z);
+
+        if(focus.transform.localScale.x > 0f)
+        {
+            focusPosition = new Vector3(focusPosition.x + leadSpace, focusPosition.y, focusPosition.z);
+        }
+        else
+        {
+            focusPosition = new Vector3(focusPosition.x - leadSpace, focusPosition.y, focusPosition.z);
+        }
+
+        //transform.position = focusPosition;
+
+        transform.position = Vector3.Lerp(transform.position, focusPosition, transitionTime * Time.deltaTime);
+	}
+}
---
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D playerRigidBody;
+
+    public bool isGrounded;
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+
+    private Animator playerAnim;
+
+	// Use this for initialization
+	void Start () {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            playerRigidBody.velocity = new Vector3(moveSpeed, playerRigidBody.velocity.y);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            playerRigidBody.velocity = new Vector3(-moveSpeed, playerRigidBody.velocity.y);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x, jumpSpeed);
+        }
+
+        playerAnim.SetFloat("Speed", Mathf.Abs(playerRigidBody.velocity.x));
+        playerAnim.SetBool("Grounded", isGrounded);
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "KillBox")
+        {
+            // placeholder
+            gameObject.SetActive(false);
+        }
+    }
+
+}
